88cb76f7aeb6e47f6e58b4a781631409c59fa65f
aali7075 <aali7075@colorado.edu.com>
2019-11-17 13:25:15 -0700

    Backend files

---
+++ server.js
@@ -0,0 +1,82 @@
+/***********************
+
+  Load Components!
+
+  Express      - A Node.js Framework
+  Body-Parser  - A tool to help use parse the data in a post request
+  Pug          - A view engine for dynamically rendering HTML pages
+  Pg-Promise   - A database tool to help use connect to our PostgreSQL database
+
+***********************/
+const fs = require('fs');
+const express = require('express'); // Add the express framework has been added
+let app = express();
+
+const bodyParser = require('body-parser'); // Add the body-parser tool has been added
+app.use(bodyParser.json());              // Add support for JSON encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
+
+const pug = require('pug'); // Add the 'pug' view engine
+
+//Create Database Connection
+const pgp = require('pg-promise')();
+
+
+/**********************
+
+  Database Connection information
+
+  host: This defines the ip address of the server hosting our database.  We'll be using localhost and run our database on our local machine (i.e. can't be access via the Internet)
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  From our previous lab, we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database.  You'll need to set a password USING THE PSQL TERMINAL THIS IS NOT A PASSWORD FOR POSTGRES USER ACCOUNT IN LINUX!
+
+**********************/
+// REMEMBER to chage the password
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'football_db',
+	user: 'postgres',
+	password: 'Yaboirahzel5!'
+};
+
+let db = pgp(dbConfig);
+
+// set the view engine to ejs
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/')); // This line is necessary for us to use relative paths and access our resources directory
+
+
+
+// Concatenates an array of objects or arrays of values, according to the template,
+// to use with insert queries. Can be used either as a class type or as a function.
+//
+// template = formatting template string
+// data = array of either objects or arrays of values
+function Inserts(template, data) {
+    if (!(this instanceof Inserts)) {
+        return new Inserts(template, data);
+    }
+    this._rawDBType = true;
+    this.formatDBType = function () {
+        return data.map(d=>'(' + pgp.as.format(template, d) + ')').join(',');
+    };
+}
+
+
+
+
+var users = [['John', 23], ['Mike', 30], ['David', 18]];
+
+
+
+db.none('INSERT INTO Users(name, age) VALUES $1', Inserts('$1, $2', users))
+    .then(data=> {
+        // OK, all records have been inserted
+    })
+    .catch(error=> {
+        // Error, no records inserted
+    });
